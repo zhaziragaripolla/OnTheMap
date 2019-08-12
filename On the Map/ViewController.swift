0e687bda49d489692f7458109bdb39176a1bed4a
Zhazira <g.zhazira@gmail.com>
2019-08-12 15:48:01 +0500

    Initial Commit

---
+++ On the Map/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  On the Map
+//
+//  Created by Zhazira Garipolla on 8/12/19.
+//  Copyright © 2019 Zhazira Garipolla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
